9920ee85e018909c1ff8490ffcb4d59724693bdc
spotted-arrow-design <spotted.arrow.design@gmail.com>
2015-02-07 13:51:30 -0500

    changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,18 @@
-$( document ).ready(function() {
-
-  //
-
+$(document).ready(function() {
+  smoothScrool(1000);
 });
+
+// smoothScroll function is applied from the document ready function
+function smoothScrool (duration) {
+  $('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
